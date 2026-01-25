b5eca47daf5cd328128e43836efc8ac5d0f30fb3
cshunton <cshunton@gmail.com>
2026-01-25 13:27:07 -0800

    push plugin requirements

--- src/test/java/com/runealytics/RuneAlyticsPluginTest.java
+++ src/test/java/com/runealytics/RuneAlyticsPluginTest.java
@@ -2,7 +2,12 @@ package com.runealytics;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import net.runelite.client.plugins.PluginDescriptor;
 
+
+@PluginDescriptor(
+        name = "RuneAlytics"
+)
 public class RuneAlyticsPluginTest
 {
 	public static void main(String[] args) throws Exception
